c03b12b9375781f0fe86d36d104f9fbe9a61afaf
Maff3x <maff3x@yahoo.it>
2015-06-29 12:16:46 +0200

    aggiornamento transizione

--- Algoritmi e Strutture Dati/src/it/unibs/asd/Transizione.java
+++ Algoritmi e Strutture Dati/src/it/unibs/asd/Transizione.java
@@ -3,34 +3,72 @@
  */
 package it.unibs.asd;
 
+
 /**
- * @author Manutenzione
- *
+ * la classe Transizione serve per implementare una transizione dell'automa
  */
 public class Transizione {
+	
+	protected Object statoPartenza, statoArrivo;
+	protected Object value;
+	protected boolean guasto;
 
-	public Transizione(Object x, Object y, Object value, boolean guasto) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * costruttore con argomenti
+	 * 
+	 * @param _statoPartenza stato di partenza della transizione
+	 * @param _statoArrivo stato di arrivo della transizione
+	 * @param _value nome della transizione
+	 * @param _guasto boolean che indica se la transizione &egrave; di guasto
+	 */
+	public Transizione(Object _statoPartenza, Object _statoArrivo, Object _value, boolean _guasto) {
+		this.statoPartenza = _statoPartenza;
+		this.statoArrivo = _statoArrivo;
+		this.value = _value;
+		this.guasto = _guasto;
+	}
+	
+	/**
+	 *  costruttore senza argomenti
+	 */
+	public Transizione() {
+		this.statoPartenza =  null;
+		this.statoArrivo = null;
+		this.value = null;
+		this.guasto = false;
 	}
 
 	public Object getStatoPartenza() {
-		// TODO Auto-generated method stub
-		return null;
+		return statoPartenza;
 	}
 
 	public Object getStatoArrivo() {
-		// TODO Auto-generated method stub
-		return null;
+		return statoArrivo;
 	}
 
 	public Object getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
   
 	public boolean isGuasto() {
-		// TODO Auto-generated method stub
-		return false;
+		return guasto;
+	}
+	
+	public boolean equals(Object t) {
+		  if (t instanceof Transizione) {
+			Transizione trans = (Transizione) t;
+			return (statoPartenza.equals(trans.statoPartenza) && statoArrivo.equals(trans.statoArrivo) && value.equals(trans.value));		
+		  }
+		  return false;
 	}
 
+	public int hashCode() {
+	    return statoPartenza.hashCode()+statoArrivo.hashCode()+value.hashCode() + Integer.parseInt(guasto + "");
+	}
+
+	public String toString() {
+		return "<"+statoPartenza.toString()+", "+statoArrivo.toString()+"; "+value.toString()+"; "+guasto+">";
+	}
+
+	
 }
