f73b9ec73066853cf6d698694ac0065cbf4197fb
Maff3x <maff3x@yahoo.it>
2015-06-29 14:45:27 +0200

    aggiunta classe CostruisciAutoma per leggere da file XML

---
+++ Algoritmi e Strutture Dati/src/it/unibs/algoritmi/CostruisciAutoma.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package it.unibs.algoritmi;
+
+import java.io.File;
+
+import it.unibs.asd.Automa;
+
+/**
+ * 
+ */
+public class CostruisciAutoma {
+	
+	public static Automa costruisciAutoma(File xmlFile){
+		Automa a = new Automa();
+		
+		
+		//si aggiungono le transizioni con a.add(....)
+		
+		return a;
+	}
+
+}
