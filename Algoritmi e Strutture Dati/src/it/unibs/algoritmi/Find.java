8f742fd138f2ea0f9b78ad8424b38497689fbb43
Maff3x <maff3x@yahoo.it>
2015-07-08 10:42:42 +0200

    Rimossa classe find, aggiunta classe Trasformazioni per algoritmi

--- Algoritmi e Strutture Dati/src/it/unibs/algoritmi/Find.java
+++
@@ -1,5 +0,0 @@
-package it.unibs.algoritmi;
-
-public class Find {
-
-}
---
+++ Algoritmi e Strutture Dati/src/it/unibs/algoritmi/Trasformazioni.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package it.unibs.algoritmi;
+
+import it.unibs.asd.Automa;
+
+/**
+ * @author Manutenzione
+ *
+ */
+public class Trasformazioni {
+	
+	/**
+	 * Trasforma il bad twin di livello 0 (l'automa di partenza) 
+	 * nel bad twin di livello 1 
+	 * che ha solo transizioni osservabili.
+	 */
+	public static Automa badtwin0to1(Automa a){
+		Automa bt = new Automa();
+		
+		
+		return bt;
+	}
+
+}
