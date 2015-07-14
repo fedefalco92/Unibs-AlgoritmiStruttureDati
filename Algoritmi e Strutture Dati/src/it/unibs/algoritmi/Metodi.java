be60b8496cca58069c3908df7ff6ba4252e756b2
Maff3x <maff3x@yahoo.it>
2015-07-14 18:19:15 +0200

    aggiunto metodo3 WIP

--- Algoritmi e Strutture Dati/src/it/unibs/algoritmi/Metodi.java
+++ Algoritmi e Strutture Dati/src/it/unibs/algoritmi/Metodi.java
@@ -85,6 +85,65 @@ public class Metodi {
 		return true;		
 	}
 	
+	public static boolean diagnosticabilitaMetodo3(Automa a, int livello){
+		
+		Automa badtwin1 = Trasformazioni.badtwin0to1(a);
+		if((badtwin1.diagnosticabilitaC2()||badtwin1.diagnosticabilitaC3())&&(livello==1)){
+			return true;
+		}
+		Automa goodtwin1 = Trasformazioni.badToGoodTwin(badtwin1);
+		Automa sincronizzato = Trasformazioni.sincronizzazione(goodtwin1, badtwin1);
+		if(sincronizzato.diagnosticabilitaC1()&&(livello==1)){
+			return true;			
+		}
+		
+		if(controllaCicli(sincronizzato)){
+			System.out.println("Il livello di diagnosticabilita' massimo e' " + 0);
+			return false;
+		} else if(livello==1) {
+			return true;
+		}
+		
+		//livello dal 2 in poi
+		/*
+		int i = 2;
+		Automa btiprev = a;
+		while (i<=livello){
+			System.out.println("*********************************************************************************************************\n");
+			Automa bti = Trasformazioni.badtwinLevelUp(btiprev, i);
+			btiprev =  bti;
+			System.out.println("Il bad twin di livello "+i+" e':\n" + bti);
+			System.out.println("L'insieme delle transizioni e': \n"+ bti.getTransizioni());
+			if(bti.diagnosticabilitaC2()||bti.diagnosticabilitaC3()){
+				System.out.println("Livello "+i+ "diagnosticabile\n");
+				i++;
+				continue;	
+			}
+			Automa gti = Trasformazioni.badToGoodTwin(bti);
+			System.out.println("Il good twin di livello "+i+" e':\n" + gti);
+			System.out.println("L'insieme delle transizioni e': \n"+ gti.getTransizioni());
+			Automa sincronizzato = Trasformazioni.sincronizzazione(gti, bti);
+			System.out.println("*******************************************************************\n");
+			System.out.println("L'automa sincronizzato livello "+i+" e':\n");
+			System.out.println("L'insieme degli stati e':\n" + sincronizzato.getStati());
+			System.out.println("\n"+ sincronizzato);
+			System.out.println("L'insieme delle transizioni e': \n"+ sincronizzato.getTransizioni());
+			if(sincronizzato.diagnosticabilitaC1()){
+				System.out.println("Livello "+i+"diagnosticabile\n");
+				i++;
+				continue;
+			}
+			
+			if (controllaCicli(sincronizzato)) {
+				System.out.println("Il livello di diagnosticabilita' massimo e' " + (i - 1));
+				return false;
+			}
+			
+			i++;
+		}	*/
+		return true;		
+	}
+	
 	/**
 	 * Controlla se esiste almeno un cammino in cui la prima transizione ambigua &egrave; seguita (anche non immediatamente) da un ciclo (infinito).
 	 * (Per definizione la negazione di questa condizione &egrave; la condizione di diagnosticabilit&agrave;)
@@ -110,6 +169,17 @@ public class Metodi {
 		return false;
 	}
 	
+	
+	/**
+	 * Visita progressivamente l'automa a partire dallo stato partenza.
+	 * Se dallo stato partenza non esce alcuna transizione si &egrave; arrivati in un punto morto.
+	 * Se dallo stato partenza esce una transizione che ha come stato di arrivo uno stato gi&agrave; visitato
+	 * in precendenza, allora si &egrave; incappati in un ciclo.
+	 * @param sincronizzato
+	 * @param visitati
+	 * @param partenza
+	 * @return true se c'&egrave; un ciclo.
+	 */
 	private static boolean visitaRicorsiva(Automa sincronizzato, Set<Stato> visitati, Stato partenza){
 		
 		Set<Transizione> uscenti = sincronizzato.getTransizioniUscenti(partenza);
